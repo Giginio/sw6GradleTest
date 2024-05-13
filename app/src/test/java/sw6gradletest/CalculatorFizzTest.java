c7abf87949efeab29f4aec82de6371aac428ca20
Giginio <gigi.margarito1@gmail.com>
2024-05-13 23:08:24 +0200

    CalculatorFizzTest

---
+++ app/src/test/java/sw6gradletest/CalculatorFizzTest.java
@@ -0,0 +1,21 @@
+package sw6gradletest;
+
+import org.junit.jupiter.api.Test;
+import org.assertj.core.api.Assertions;
+
+public class CalculatorFizzTest {
+    @Test
+    public void testMultiply() {
+        CalculatorFizz fizzBuzz = new CalculatorFizz();
+        Assertions.assertThat(fizzBuzz.multiply(1, 9)).isEqualTo("Fizz");
+        Assertions.assertThat(fizzBuzz.multiply(3, 7)).isEqualTo("FizzBuzz");
+        Assertions.assertThat(fizzBuzz.multiply(8, 7)).isEqualTo("Buzz");
+        Assertions.assertThat(fizzBuzz.multiply(1, 2)).isEqualTo("2");
+        Assertions.assertThat(fizzBuzz.multiply(3, 9)).isEqualTo("Fizz");
+        Assertions.assertThat(fizzBuzz.multiply(3, 7)).isEqualTo("FizzBuzz");
+        Assertions.assertThat(fizzBuzz.multiply(2, 9)).isEqualTo("Fizz");
+        Assertions.assertThat(fizzBuzz.multiply(6, 7)).isEqualTo("2");
+        Assertions.assertThat(fizzBuzz.multiply(1, 7)).isEqualTo("Buzz");
+        Assertions.assertThat(fizzBuzz.multiply(2, 2)).isEqualTo("4");
+        Assertions.assertThat(fizzBuzz.multiply(4, 2)).isEqualTo("8");
+    }
