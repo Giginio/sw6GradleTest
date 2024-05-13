d9a8d709332f28234606254ffad9830975ca555f
Giginio <gigi.margarito1@gmail.com>
2024-05-13 23:41:02 +0200

    new test

--- app/src/test/java/sw6gradletest/CalculatorFizzTest.java
+++ app/src/test/java/sw6gradletest/CalculatorFizzTest.java
@@ -4,18 +4,33 @@ import org.junit.jupiter.api.Test;
 import org.assertj.core.api.Assertions;
 
 public class CalculatorFizzTest {
+
     @Test
-    public void testMultiply() {
+    public void testMultiplyFizz() {
         CalculatorFizz fizzBuzz = new CalculatorFizz();
         Assertions.assertThat(fizzBuzz.multiply(1, 9)).isEqualTo("Fizz");
-        Assertions.assertThat(fizzBuzz.multiply(3, 7)).isEqualTo("FizzBuzz");
-        Assertions.assertThat(fizzBuzz.multiply(8, 7)).isEqualTo("Buzz");
-        Assertions.assertThat(fizzBuzz.multiply(1, 2)).isEqualTo("2");
         Assertions.assertThat(fizzBuzz.multiply(3, 9)).isEqualTo("Fizz");
-        Assertions.assertThat(fizzBuzz.multiply(3, 7)).isEqualTo("FizzBuzz");
         Assertions.assertThat(fizzBuzz.multiply(2, 9)).isEqualTo("Fizz");
-        Assertions.assertThat(fizzBuzz.multiply(6, 7)).isEqualTo("2");
+    }
+
+    @Test
+    public void testMultiplyBuzz() {
+        CalculatorFizz fizzBuzz = new CalculatorFizz();
+        Assertions.assertThat(fizzBuzz.multiply(8, 7)).isEqualTo("Buzz");
         Assertions.assertThat(fizzBuzz.multiply(1, 7)).isEqualTo("Buzz");
+    }
+
+    @Test
+    public void testMultiplyFizzBuzz() {
+        CalculatorFizz fizzBuzz = new CalculatorFizz();
+        Assertions.assertThat(fizzBuzz.multiply(3, 7)).isEqualTo("FizzBuzz");
+        Assertions.assertThat(fizzBuzz.multiply(6, 7)).isEqualTo("FizzBuzz");
+    }
+
+    @Test
+    public void testMultiplyNumbers() {
+        CalculatorFizz fizzBuzz = new CalculatorFizz();
+        Assertions.assertThat(fizzBuzz.multiply(1, 2)).isEqualTo("Fizz");
         Assertions.assertThat(fizzBuzz.multiply(2, 2)).isEqualTo("4");
         Assertions.assertThat(fizzBuzz.multiply(4, 2)).isEqualTo("8");
     }
